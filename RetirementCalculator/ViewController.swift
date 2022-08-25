e0f0cfaa4732695f28b32918e5792cfa312f6a63
Vitor Campos <vitorcampos@brq.com>
2022-08-25 16:32:03 -0300

    Add feature 1

--- RetirementCalculator/ViewController.swift
+++ RetirementCalculator/ViewController.swift
@@ -16,6 +16,7 @@ class ViewController: UIViewController {
     @IBOutlet weak var fourthField: UITextField!
     @IBOutlet weak var fifthField: UITextField!
     @IBOutlet weak var calculateButton: UIButton!
+    @IBOutlet weak var resultLabel: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
@@ -32,10 +33,14 @@ class ViewController: UIViewController {
     }
 
     @IBAction func click(_ sender: Any) {
-        guard let currentAge = secondField.text else { return }
-        guard let planned_retirement_age = thirdField.text else { return }
+        guard let monthlyInvestmint: Int = Int(firstField.text!) else { return }
+        guard let currentAge = Int(secondField.text!) else { return }
+        guard let planned_retirement_age = Int(thirdField.text!) else { return }
+        guard let average_interest_rate = Int(fourthField.text!)  else { return }
+        guard let currentSavings = Int(fifthField.text!) else { return }
+        resultLabel.text = "If you save $\(monthlyInvestmint) every month for \(planned_retirement_age - currentAge) years, and invest that money plus your current investment of \(currentSavings) at a \(average_interest_rate)% anual interest rate, you will have X by th time you are \(planned_retirement_age)"
         
-        let properties = ["current_age" : currentAge, "planned_retirement_age" : planned_retirement_age]
+        let properties = ["current_age" : String(currentAge), "planned_retirement_age" : String(planned_retirement_age)]
         
         Analytics.trackEvent("Calculate_retirement_amount", withProperties: properties)
     }
